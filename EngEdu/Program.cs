908ec13cd83991a5b190fa7797698426f3ad6258
“USER <bom_rsl4@hotmail.com>
2017-08-31 14:27:03 +0700

    (Computer) I Start learn HTML

--- EngEdu/Program.cs
+++ EngEdu/Program.cs
@@ -14,6 +14,7 @@ namespace EngEdu
             Console.WriteLine("Year 255x semester 2,I learn to program in c sharp.");
             Console.WriteLine("Year 2560 semester 1,I start my programmer life.");
             Console.WriteLine("Year 2560 semester 2,I learn to data bate. ");
+            Console.WriteLine("Year 2561 semester 1,I Start learn HTML.");
             
 
         }
